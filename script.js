44173fba19397c2595511f815c0b7c68e29f5df4
Kaleb Lamar <lamarkaleb@gmail.com>
2023-03-17 08:54:14 -0500

    LiteralMathmethodExercise

---
+++ script.js
@@ -0,0 +1,47 @@
+//1a
+let warmHugs = "hi, I'm Olaf and like warm hugs ";
+console.log(warmHugs);
+
+//1b
+console.log(warmHugs.toUpperCase());
+
+//1c
+warmHugs = warmHugs.replace("like", "love");
+console.log(warmHugs.replace("like", "love"));
+console.log(warmHugs);
+
+//2a
+let beenImpaled = "oh, look at that.I've been impaled.";
+console.log(beenImpaled);
+
+//2b
+console.log(beenImpaled.slice(17));
+//3a
+let dotDotDot = "....";
+//3b
+let skullBones = `"I don't have a skull ${dotDotDot} or bones"`;
+console.log(skullBones);
+
+//4
+console.log(Math.PI);
+
+//5
+let randomNumber = Math.random();
+randomNumber *= 3;
+console.log(randomNumber);
+
+//BONUS
+//6
+let letItGo = "'Let it Go!'";
+console.log(letItGo.repeat(2));
+
+//7
+let reindeers = "Reindeer are better than people.";
+console.log(reindeers.replaceAll(" ", "_"));
+
+//8
+console.log(Math.sqrt(2));
+
+//9
+let newRandomNumber = Math.floor(Math.random() * 17) + 7;
+console.log(newRandomNumber);
